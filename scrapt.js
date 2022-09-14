bd0e66a15395dd472614b170deb4380b4cdc69a4
Abdualrhman-Nagah <90969646+Abdualrhman-Nagah@users.noreply.github.com>
2022-09-14 05:05:34 +0200

    Add files via upload

---
+++ scrapt.js
@@ -0,0 +1,218 @@
+let click = document.querySelector('.click');
+
+let list = document.querySelector('.list');
+
+click.addEventListener("click",()=>{
+
+    list.classList.toggle('newlist');
+
+});
+
+let click1 = document.querySelector('.links');
+
+let list1 = document.querySelector('.more-list');
+
+click1.addEventListener("click",()=>{
+
+    list1.classList.toggle('newlist');
+
+});
+
+let click2 = document.querySelector('.links2');
+
+let list2 = document.querySelector('.more-list2');
+
+click2.addEventListener("click",()=>{
+
+    list2.classList.toggle('newlist');
+
+});
+
+
+let click3 = document.querySelector('.links3');
+
+let list3 = document.querySelector('.more-list3');
+
+click3.addEventListener("click",()=>{
+
+    list3.classList.toggle('newlist');
+
+});
+
+let click4 = document.querySelector('.links4');
+
+let list4 = document.querySelector('.more-list4');
+
+click4.addEventListener("click",()=>{
+
+    list4.classList.toggle('newlist');
+
+});
+
+let click5 = document.querySelector('.links5');
+
+let list5 = document.querySelector('.more-list5');
+
+click5.addEventListener("click",()=>{
+
+    list5.classList.toggle('newlist');
+
+});
+
+let click6 = document.querySelector('.links6');
+
+let list6 = document.querySelector('.more-list6');
+
+click6.addEventListener("click",()=>{
+
+    list6.classList.toggle('newlist');
+
+});
+
+let click7 = document.querySelector('.links7');
+
+let list7 = document.querySelector('.more-list7');
+
+click7.addEventListener("click",()=>{
+
+    list7.classList.toggle('newlist');
+
+});
+
+/*************************************** */
+let click8 = document.querySelector('.links8');
+
+let list8 = document.querySelector('.more-list8');
+
+click8.addEventListener("click",()=>{
+
+    list8.classList.toggle('newlist');
+
+});
+let click9 = document.querySelector('.links9');
+
+let list9 = document.querySelector('.more-list9');
+
+click9.addEventListener("click",()=>{
+
+    list9.classList.toggle('newlist');
+
+});
+let click10 = document.querySelector('.links10');
+
+let list10 = document.querySelector('.more-list10');
+
+click10.addEventListener("click",()=>{
+
+    list10.classList.toggle('newlist');
+
+});
+let click11 = document.querySelector('.links11');
+
+let list11 = document.querySelector('.more-list11');
+
+click11.addEventListener("click",()=>{
+
+    list11.classList.toggle('newlist');
+
+});
+let click12 = document.querySelector('.links12');
+
+let list12 = document.querySelector('.more-list12');
+
+click12.addEventListener("click",()=>{
+
+    list12.classList.toggle('newlist');
+
+});
+let click13 = document.querySelector('.links13');
+
+let list13 = document.querySelector('.more-list13');
+
+click13.addEventListener("click",()=>{
+
+    list13.classList.toggle('newlist');
+
+});
+let click14 = document.querySelector('.links14');
+
+let list14 = document.querySelector('.more-list14');
+
+click14.addEventListener("click",()=>{
+
+    list14.classList.toggle('newlist');
+
+});
+let click15 = document.querySelector('.links15');
+
+let list15 = document.querySelector('.more-list15');
+
+click15.addEventListener("click",()=>{
+
+    list15.classList.toggle('newlist');
+
+});
+let click16 = document.querySelector('.links16');
+
+let list16 = document.querySelector('.more-list16');
+
+click16.addEventListener("click",()=>{
+
+    list16.classList.toggle('newlist');
+
+});
+let click17 = document.querySelector('.links17');
+
+let list17 = document.querySelector('.more-list17');
+
+click17.addEventListener("click",()=>{
+
+    list17.classList.toggle('newlist');
+
+});
+let click18 = document.querySelector('.links18');
+
+let list18 = document.querySelector('.more-list18');
+
+click18.addEventListener("click",()=>{
+
+    list18.classList.toggle('newlist');
+
+});
+let click19 = document.querySelector('.links19');
+
+let list19 = document.querySelector('.more-list19');
+
+click19.addEventListener("click",()=>{
+
+    list19.classList.toggle('newlist');
+
+});
+let click20 = document.querySelector('.links20');
+
+let list20 = document.querySelector('.more-list20');
+
+click20.addEventListener("click",()=>{
+
+    list20.classList.toggle('newlist');
+
+});
+/*
+let click7 = document.querySelector('.links7');
+
+let list7 = document.querySelector('.more-list7');
+
+click7.addEventListener("click",()=>{
+
+    list7.classList.toggle('newlist');
+
+});
+let click7 = document.querySelector('.links7');
+
+let list7 = document.querySelector('.more-list7');
+
+click7.addEventListener("click",()=>{
+
+    list7.classList.toggle('newlist');
+
+});*/
